e5452844626da22c9f2759d657cd0048830e66d9
Erick José da Silva Alencar <erick.alencar@unicsim.com>
2024-06-25 15:38:25 -0300

    Novo método e inversion of control

--- OrderApp/src/main/java/com/techrat/OrderApp/resources/UserResource.java
+++ OrderApp/src/main/java/com/techrat/OrderApp/resources/UserResource.java
@@ -1,19 +1,32 @@
 package com.techrat.OrderApp.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.techrat.OrderApp.entities.User;
+import com.techrat.OrderApp.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService _service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User testUser = new User(1L, "Mary", "Mary@gmail.com", "9999999", "12345");
-		return ResponseEntity.ok().body(testUser);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = _service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	@GetMapping(value="/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = _service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
